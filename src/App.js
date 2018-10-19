6953875ef83285bec7ce89c7140aa283d7949853
Roman Zanettin <roman.zanettin@gmail.com>
2018-10-19 22:06:56 +0200

    removed yarn.lockremoved yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from "inferno";
 import Button from "./components/Button";
 import InputField from "./components/InputField";
+import compose from "incompose/dist/compose";
+import withState from "incompose/dist/withState";
 
 class App extends Component {
   onClick = () => {
@@ -13,9 +15,10 @@ class App extends Component {
         <h1>Inferno Parcel boilerplate Example</h1>
         <InputField value="toto" ref={el => (this.input = el)} />
         <Button onClick={this.onClick} label="push" />
+        <h3>{this.props.a}</h3>
       </div>
     );
   }
 }
 
-export default App;
+export default compose(withState("a", "setA", 999))(App);
